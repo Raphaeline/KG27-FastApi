e4736fdf59d6906a16d99cb35ce30d26dd9f2f5d
Raphaeline <zulfikhar.akbar1412@gmail.com>
2026-04-16 15:20:29 +0700

    add features for auto renaming result data

--- api/index.py
+++ api/index.py
@@ -3,12 +3,12 @@ import io
 import pandas as pd
 from fastapi.responses import StreamingResponse
 from fastapi import FastAPI, UploadFile, File, HTTPException
+from datetime import datetime
 import sys
 import os
 
-# penting untuk Vercel (fix import path)
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 app = FastAPI(
     title="KG27 Hex Parser API",
@@ -17,13 +17,11 @@ app = FastAPI(
 )
 
 
-# @app.get("/")
-# def root():
-#     return {"status": "API running on Vercel"}
-
-
 @app.post("/parser", tags=["Processing"])
 async def process_csv(file: UploadFile = File(...)):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+
+    filename = f"result_{timestamp}.xlsx"
 
     if not file.filename or not file.filename.lower().endswith(".csv"):
         raise HTTPException(status_code=400, detail="File harus CSV")
@@ -35,7 +33,6 @@ async def process_csv(file: UploadFile = File(...)):
     if 'custom.235.hex' not in df.columns:
         raise HTTPException(
             status_code=400, detail="Kolom custom.235.hex tidak ditemukan")
-
     try:
         df = process_dataframe(df)
     except Exception as e:
@@ -47,12 +44,10 @@ async def process_csv(file: UploadFile = File(...)):
         output.seek(0)
     except Exception:
         raise HTTPException(status_code=500, detail="Gagal convert ke Excel")
-
-    # 6. Return file
     return StreamingResponse(
         output,
         media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
         headers={
-            "Content-Disposition": "attachment; filename=hasil_parsing.xlsx"
+            "Content-Disposition": f"attachment; filename={filename}"
         }
     )
